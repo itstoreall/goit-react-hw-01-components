666c4fa44d7df60291eb9d851bf035da04719b30
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-14 23:45:20 +0200

    Убил импорт

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import { name, tag, location, avatar, stats } from './user.json';
-import statisticalData from './statistical-data.json';
+// import statisticalData from './statistical-data.json';
 import friends from './friends.json';
 import transactions from './transactions.json';
 
