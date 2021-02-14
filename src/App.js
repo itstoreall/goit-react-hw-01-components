52d8e64a1dd2eadb203b1943044c58ab83941780
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-14 23:08:55 +0200

    lint test4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import { name, tag, location, avatar, stats } from './user.json';
 import statisticalData from './statistical-data.json';
-import friends from './friends.json';
+// import friends from './friends.json';
 import transactions from './transactions.json';
 
 const App = () => (
